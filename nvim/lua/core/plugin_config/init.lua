7ec25e758d77b496f49aca406d5eed81c506fb7c
Dmitry A <dmitry.astapenko@gmail.com>
2024-04-14 15:55:48 +0200

    Use new nvim config

---
+++ nvim/lua/core/plugin_config/init.lua
@@ -0,0 +1,6 @@
+require("core.plugin_config.gruvbox")
+require("core.plugin_config.nvimtree_config")
+require("core.plugin_config.lualine")
+require("core.plugin_config.treesitter")
+require("core.plugin_config.telescope")
+require("core.plugin_config.lsp_config")
